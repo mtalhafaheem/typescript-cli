39b691674016109fb92be310401a40e1a6f590de
mtalhafaheem <talha.faheem@apimatic.io>
2025-07-16 12:24:45 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Function to calculate the sum of two numbers
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(7, 5);
+console.log(`The sum of 7 and 5 is: ${result}`);
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Function to calculate the sum of two numbers
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+const result = sum(7, 5);
+console.log(`The sum of 7 and 5 is: ${result}`); 
